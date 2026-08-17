9f47c788533ffaa70ca93009095eb68215f37897
Seoyeon Choi <westion28we@gmail.com>
2026-08-17 11:14:50 +0900

    [level 0] Title: 0 떼기, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -1,15 +1,2 @@
 def solution(n_str):
-    answer=''
-    still_zero=True
-    for string in n_str:
-        if string == '0':
-            if still_zero:
-                pass
-            else:
-                answer+=str(string)
-        else:
-            answer+=str(string)
-            still_zero=False
-    return answer
-                
-    return answer
+    return n_str.lstrip('0')
